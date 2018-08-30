eb05f7e4393a6a20dff66b230e1eaa29f6ad413e
bdOlson <elloderrick@hotmail.com>
2018-08-30 11:40:52 -0500

    Added stuff to get xml working

--- app/src/main/java/com/troot/dmstats/monsterListScroll.java
+++ app/src/main/java/com/troot/dmstats/monsterListScroll.java
@@ -2,8 +2,34 @@ package com.troot.dmstats;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.Environment;
 
-public class monsterListScroll extends Activity {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public abstract class monsterListScroll extends Activity {
+
+    // Call the monsterXMLParser method in monsterXMLParser.java
+    monsterXMLParser xmlParser = new monsterXMLParser();
+
+
+
+/* Can't use this under API 26, we need to allow down to 15
+    Path path = Paths.get("values\\monster.xml");
+    File file = path.toFile();
+*/
+
+    //InputStream xmlFileInSt =
+
+
+    // Create parse object from parse method in monsterXMLParser.java
+    List parse = xmlParser.parse(xmlFileInSt); // Need to grab monster.xml
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
